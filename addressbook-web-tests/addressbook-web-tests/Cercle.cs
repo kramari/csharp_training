a941a2d4ca3642fb7cd25f0413dfb1c744d154d3
kramari <nezhnayka@mail.ru>
2018-04-04 00:26:50 +0300

    Задание 3 выполнено

---
+++ addressbook-web-tests/addressbook-web-tests/Cercle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_web_tests
+{
+    class Cercle : Figure
+    {
+        private int radius;
+       
+        public Cercle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public int Radius
+        {
+            get
+            {
+                return radius;
+            }
+
+            set
+            {
+                radius = value;
+            }
+        }
+                
+    }
+}
--- addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
+++ addressbook-web-tests/addressbook-web-tests/UnitTest1.cs
@@ -7,8 +7,38 @@ namespace addressbook_web_tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethodSquare()
         {
+            Square s1 = new Square(5);
+            Square s2 = new Square(10);
+            Square s3 = s1;
+
+            Assert.AreEqual(s1.Size, 5);
+            Assert.AreEqual(s2.Size, 10);
+            Assert.AreEqual(s3.Size, 5);
+
+            s3.Size = 15;
+
+            Assert.AreEqual(s1.Size, 15);
+
+            s2.Colored = true;
+        }
+
+        [TestMethod]
+        public void TestMethodCercle()
+        {
+            Cercle s1 = new Cercle(5);
+            Cercle s2 = new Cercle(10);
+            Cercle s3 = s1;
+
+            Assert.AreEqual(s1.Radius, 5);
+            Assert.AreEqual(s2.Radius, 10);
+            Assert.AreEqual(s3.Radius, 5);
+
+            s3.Radius = 15;
+
+            Assert.AreEqual(s1.Radius, 15);
+            s2.Colored = true;
         }
     }
 }
