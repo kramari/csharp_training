9a706f16d32d067516597524fc36c902c7d4ce43
kramari <nezhnayka@mail.ru>
2018-04-04 22:48:32 +0300

    Контакт создается с незаполненными полями

---
+++ addressbook-web-tests/addressbook-web-tests/ContactData.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string firstname;
+        private string lastname;
+        private string middlename = "";
+        private string nickname = "";
+        private string title = "";
+        private string company = "";
+        private string address = "";
+        private string home = "";
+        private string email = "";
+
+        public ContactData(string firstname, string lastname)
+        {
+            this.firstname = firstname;
+            this.lastname = lastname;
+        }
+
+        public string Firstname
+        {
+            get
+            {
+                return firstname;
+            }
+            set
+            {
+                firstname = value;
+            }
+        }
+
+        public string Lastname
+        {
+            get
+            {
+                return lastname;
+            }
+            set
+            {
+                lastname = value;
+            }
+        }
+        public string Middlename
+        {
+            get
+            {
+                return middlename;
+            }
+            set
+            {
+                middlename = value;
+            }
+        }
+        public string Nickname
+        {
+            get
+            {
+                return nickname;
+            }
+            set
+            {
+                nickname = value;
+            }
+        }
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+        public string Company
+        {
+            get
+            {
+                return company;
+            }
+            set
+            {
+                company = value;
+            }
+        }
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+            set
+            {
+                address = value;
+            }
+        }
+        public string Home
+        {
+            get
+            {
+                return home;
+            }
+            set
+            {
+                home = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+    }
+}
